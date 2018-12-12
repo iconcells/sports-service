108bc33408bda0bef29c414813a0e97e9858f670
Kenneth Ng <ngkenneth@gmail.com>
2018-12-11 20:11:03 -0800

    added

--- json_import.py
+++ json_import.py
@@ -1,5 +1,6 @@
 import json
-import requests 
+import requests
+import bolo
 from subprocess import call
 
 with open("test.json") as data_file:
@@ -10,6 +11,9 @@ with open("test.json") as data_file:
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
 todos = json.loads(response.text)
+
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
 
 print json.dumps(todos, sort_keys=True,
 				indent=4, separators=(',', ': '))
